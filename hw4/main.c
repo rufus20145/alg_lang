e92ca76ac36e76b97ff0396d60bdce74adfbc5e6
LAPTOP20145 <ivan20027749@gmail.com>
2020-09-13 17:41:05 +0300

    add text

--- hw4/main.c
+++ hw4/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
+
+//some command line arguments
 int main(int argc, char* argv[]) {
     for(int i = argc; i >= 0; i--) {
         printf("%s\n", argv[i]);
