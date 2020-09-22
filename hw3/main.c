15e7584504f97f6cea47175e51231b6293261d00
LAPTOP20145 <ivan20027749@gmail.com>
2020-09-22 13:03:07 +0300

    before 09.22

--- hw3/main.c
+++ hw3/main.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
+#include <string.h>
+
 
 int main() {
     int num1, num2;
-    char letter[1];
+    char letter[3];
     printf("Hello!\n");
     do {
         printf("Please enter two numbers:\n");
         scanf("%d %d", &num1, &num2);
-        printf("Sum is %d\n", num1 + num2);
-        printf("Do you want to continue? (Y or N)\n");
-        scanf("%1s", &letter[1]);
+        if(num1 > num2) printf("Num1 is greater than num2 by %d.\n", num1 - num2);
+        else printf("Num2 is greater than num1, their sum is: %d.\n", num1 + num2);
+        printf("Do you want to continue? (YES or NO). Default is YES\n");
+        scanf("%3s", letter);// %c doesn`t work
+        // if(letter == 'n') break;
     }
-    while (letter[1] == 'Y');
+    while (!strcmp(letter, "YES"));//two conditions at the same time doesn`t work - ((letter != 'N') || (letter != 'n'))
     printf("Bye!\n");
     return 0;
 }
